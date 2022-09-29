acae7873f06d83b95a43b09d0e7440936ff71b61
oishbdh <ikechukwuarinze614@gmail.com>
2022-09-29 19:36:30 +0100

    Calculator_vue

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <router-link to="/CalcDev"></router-link>
+  <CalcDev/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CalcDev from './components/CalcDev.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CalcDev
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/CalcDev.vue
@@ -0,0 +1,79 @@
+<template>
+    <div class="calculator">
+        <div class="display">{{current || '0'}}</div>
+        <div @click="clear" class="btn">C</div>
+        <div @click="sign" class="btn">+/-</div>
+        <div class="btn">%</div>
+        <div class="btn operator">/</div>
+        <div class="btn">7</div>
+        <div class="btn">8</div>
+        <div class="btn">9</div>
+        <div class="btn operator">*</div>
+        <div class="btn">4</div>
+        <div class="btn">5</div>
+        <div class="btn">6</div>
+        <div class="btn operator">-</div>
+        <div class="btn">1</div>
+        <div class="btn">2</div>
+        <div class="btn">3</div>
+        <div class="btn operator">+</div>
+        <div class="btn zero">0</div>
+        <div class="btn">.</div>
+        <div class="btn operator">=</div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    data() {
+        return {
+            current: '1234',
+        }
+        
+    },
+    /*******To define function you add a method */
+
+    methods: {
+        clear() {
+            this.current = '';
+        },
+        sign() {
+            this.current = this.current.charAt(0) === '-'?
+              this.current.slice(1) : '-';
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .calculator{
+        width: 400px;
+        margin: 0 auto;
+        font-size: 40px;
+        display: grid;
+        grid-template-columns: repeat(4, 1fr);
+        grid-auto-rows: minmax(50px, auto);
+    }
+
+    .display{
+        grid-column: 1 / 5;
+        background-color: #333;
+        color: #ffff;
+    }
+
+    .zero{
+        grid-column: 1 / 3;
+    }
+
+    .btn{
+        background-color: #F2F2F2;
+        border: 1px solid #3333;
+    }
+
+    .operator{
+        background-color: orange;
+        color: white;
+    }
+
+</style>
